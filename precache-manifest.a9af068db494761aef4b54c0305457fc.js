b7f69f715d008eb1c34c3f36b1bba78defc962a5
Julius Williams <jmichaelcodes@gmail.com>
2018-10-30 15:32:36 -0400

    Updates

---
+++ precache-manifest.a9af068db494761aef4b54c0305457fc.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fedc2eef6ab593c9eac7fa4dcf5fce9",
+    "url": "/jmichaelcodes-react/static/media/nosh_1.4fedc2ee.png"
+  },
+  {
+    "revision": "badf0e164f64a137cb49",
+    "url": "/jmichaelcodes-react/static/css/main.19e53bb5.chunk.css"
+  },
+  {
+    "revision": "8bef500a8417902704c2",
+    "url": "/jmichaelcodes-react/static/js/1.8bef500a.chunk.js"
+  },
+  {
+    "revision": "6f2f312a56eaf91d3144",
+    "url": "/jmichaelcodes-react/static/js/runtime~main.6f2f312a.js"
+  },
+  {
+    "revision": "e3efe24d038937a8050aa269946c89c5",
+    "url": "/jmichaelcodes-react/static/media/android.e3efe24d.png"
+  },
+  {
+    "revision": "b2f7b0c5b541c73d3981d1bf33d871bf",
+    "url": "/jmichaelcodes-react/static/media/css.b2f7b0c5.png"
+  },
+  {
+    "revision": "8b6658abf75e5e475fac8f5e017f7768",
+    "url": "/jmichaelcodes-react/static/media/java.8b6658ab.png"
+  },
+  {
+    "revision": "0dc5bb89d598bd674f67a710310b31ce",
+    "url": "/jmichaelcodes-react/static/media/javascript.0dc5bb89.png"
+  },
+  {
+    "revision": "1a1882b8869d04e6b7dce7245f074bb0",
+    "url": "/jmichaelcodes-react/static/media/jquery.1a1882b8.png"
+  },
+  {
+    "revision": "a2b57171ae3c9f4ff5055e2db4fbd675",
+    "url": "/jmichaelcodes-react/static/media/react-native.a2b57171.png"
+  },
+  {
+    "revision": "c03ff3e17645426cd725144c594b4041",
+    "url": "/jmichaelcodes-react/static/media/react.c03ff3e1.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jmichaelcodes-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "040f401842f9564948f3d4bd25d2d703",
+    "url": "/jmichaelcodes-react/static/media/dragos_1.040f4018.png"
+  },
+  {
+    "revision": "0cf74a965c3b53462897584424f91511",
+    "url": "/jmichaelcodes-react/static/media/dragos_2.0cf74a96.png"
+  },
+  {
+    "revision": "6d2dd5e701882f8af7da3065579d6735",
+    "url": "/jmichaelcodes-react/static/media/dragos_3.6d2dd5e7.png"
+  },
+  {
+    "revision": "b2eebeaebf6e7bbf3551eb8d853f836d",
+    "url": "/jmichaelcodes-react/static/media/kyne_1.b2eebeae.png"
+  },
+  {
+    "revision": "f5e878408911a901a2039ff97fa2fc2d",
+    "url": "/jmichaelcodes-react/static/media/kyne_2.f5e87840.png"
+  },
+  {
+    "revision": "64776c0ed856ceb861feb3025d99c045",
+    "url": "/jmichaelcodes-react/static/media/kyne_3.64776c0e.png"
+  },
+  {
+    "revision": "8e3a025d0544847e65d67fd69187a6bd",
+    "url": "/jmichaelcodes-react/static/media/kyne_4.8e3a025d.png"
+  },
+  {
+    "revision": "badf0e164f64a137cb49",
+    "url": "/jmichaelcodes-react/static/js/main.badf0e16.chunk.js"
+  },
+  {
+    "revision": "2d02b640cd777f3ed745cd21a30c363a",
+    "url": "/jmichaelcodes-react/static/media/nosh_2.2d02b640.png"
+  },
+  {
+    "revision": "a6264355c5c52c7ca0846b44ef48be66",
+    "url": "/jmichaelcodes-react/static/media/nosh_3.a6264355.png"
+  },
+  {
+    "revision": "a88f2fdeb49282211bf551e46ceef6ca",
+    "url": "/jmichaelcodes-react/static/media/ntfxn_1.a88f2fde.png"
+  },
+  {
+    "revision": "4b67d5c208a889bb6a5c874745db3ed1",
+    "url": "/jmichaelcodes-react/static/media/ntfxn_2.4b67d5c2.png"
+  },
+  {
+    "revision": "293e95d5be92f72500c532ddfca634c8",
+    "url": "/jmichaelcodes-react/static/media/ntfxn_3.293e95d5.png"
+  },
+  {
+    "revision": "f92475072690e2fc6b7a40f3bedcbaa1",
+    "url": "/jmichaelcodes-react/static/media/ludm_1.f9247507.png"
+  },
+  {
+    "revision": "58cb5eeddf07615e9f92031824fa8f90",
+    "url": "/jmichaelcodes-react/static/media/ludm_2.58cb5eed.png"
+  },
+  {
+    "revision": "d856a6a15ba49409ee76564d888937f6",
+    "url": "/jmichaelcodes-react/static/media/skinnyties_1.d856a6a1.png"
+  },
+  {
+    "revision": "9006e397706cc36c800a5552197cb858",
+    "url": "/jmichaelcodes-react/static/media/skinnyties_2.9006e397.png"
+  },
+  {
+    "revision": "e5df1cd84dfc6db4b285ef565bab35ca",
+    "url": "/jmichaelcodes-react/static/media/sockorama_1.e5df1cd8.png"
+  },
+  {
+    "revision": "0763c715a23c14c80f27b964f0f861e0",
+    "url": "/jmichaelcodes-react/static/media/sockorama_2.0763c715.png"
+  },
+  {
+    "revision": "951130d39c6624b7dad5d738bad4a585",
+    "url": "/jmichaelcodes-react/static/media/surfpaddle_1.951130d3.png"
+  },
+  {
+    "revision": "8be2470057c0f5932186a6cbacd23ae3",
+    "url": "/jmichaelcodes-react/static/media/surfpaddle_2.8be24700.png"
+  },
+  {
+    "revision": "3c263cfebef37f072051c8bd4c33e188",
+    "url": "/jmichaelcodes-react/static/media/coachstr_1.3c263cfe.png"
+  },
+  {
+    "revision": "e8e3e9d7b788762288308ce0e5df197d",
+    "url": "/jmichaelcodes-react/static/media/coachstr_2.e8e3e9d7.png"
+  },
+  {
+    "revision": "bfbada1fb194945fbfdfc3058728892e",
+    "url": "/jmichaelcodes-react/static/media/coachstr_3.bfbada1f.png"
+  },
+  {
+    "revision": "28d2bf833d766df6aae2d9a8adb34058",
+    "url": "/jmichaelcodes-react/static/media/resume2018.28d2bf83.pdf"
+  },
+  {
+    "revision": "9abfb08db886d34f9bdd54ec69efb079",
+    "url": "/jmichaelcodes-react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/jmichaelcodes-react/precache-manifest.a9af068db494761aef4b54c0305457fc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/jmichaelcodes-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
